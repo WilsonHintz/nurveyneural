9ae57d98d0fda4b40a8fbf8e3f33d74bdbc11fb9
Unknown <guillermohintz@gmail.com>
2017-10-10 20:22:45 -0300

    algunos modelos que andan

--- tests/nurvey10fold.py
+++ tests/nurvey10fold.py
@@ -6,10 +6,12 @@ from sklearn.model_selection import cross_val_score
 import numpy
 
 # Function to create model, required for KerasClassifier
+
 def create_model():
     # create model
     model = Sequential()
-    model.add(Dense(24, input_dim=23, activation='relu'))
+    model.add(Dense(24, input_dim=24, activation='relu'))
+    model.add(Dense(12, activation='relu'))
     model.add(Dense(12, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     # Compile model
@@ -22,12 +24,16 @@ numpy.random.seed(seed)
 # load dataset
 dataset = numpy.loadtxt("nurvey.csv", delimiter=",")
 # split into input (X) and output (Y) variables
-X = dataset[:, 0:23]
+X = dataset[:, 0:24]
 Y = dataset[:, 24]
+
+# SinmiembrosniaportanteniedadconNSEclasificado 21
+# ConNSE(clasificado123) 24
+
 
 # create model
 model = KerasClassifier(build_fn=create_model, epochs=50, batch_size=10)
 # evaluate using 10-fold cross validation
-kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 results = cross_val_score(model, X, Y, cv=kfold)
 print(results.mean())
--- tests/nurveyneural.py
+++ tests/nurveyneural.py
@@ -4,31 +4,32 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 # seed for reproducing same results
-seed = 9
+seed = 8
 np.random.seed(seed)
 
 # load  dataset
-dataset = np.loadtxt('nurveyNoEdad.csv', delimiter=',')
+dataset = np.loadtxt('SinmiembrosniaportanteniedadconNSEclasificado.csv', delimiter=',')
 
 # split into input and output variables
-X = dataset[:,0:22]
-Y = dataset[:,23]
+X = dataset[:,0:21]
+Y = dataset[:,21]
 
 # split the data into training (67%) and testing (33%)
 (X_train, X_test, Y_train, Y_test) = train_test_split(X, Y, test_size=0.15, random_state=seed)
 
 # create the model
 model = Sequential()
-model.add(Dense(22, input_dim=22, init='uniform', activation='relu'))
-model.add(Dense(12, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(30, input_dim=21, init='uniform', activation='relu'))
+model.add(Dense(20, init='uniform', activation='relu'))
+model.add(Dense(10, init='uniform', activation='relu'))
+model.add(Dense(5, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 
 # compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # fit the model
-model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=50, batch_size=5)
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=100, batch_size=10)
 
 # evaluate the model
 scores = model.evaluate(X_test, Y_test)
