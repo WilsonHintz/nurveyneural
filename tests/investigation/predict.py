6da5f070dfd829b7c4074376bf252b45ae89767e
wilson <guillermohintz@gmail.com>
2018-10-18 09:18:58 -0300

    avances

--- tests/investigation/predict.py
+++ tests/investigation/predict.py
@@ -4,6 +4,7 @@ from keras.models import model_from_json
 json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
+
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
 loaded_model.load_weights("model.h5")
@@ -11,7 +12,7 @@ print("Loaded model from disk")
 
 x = "2;47;0;2;2;1;1;1;1;1;0;0;0;0;0;0;1;1;0;0;0;0;0;1"
 #dataset = numpy.loadtxt(x, delimiter=';')
-dataset = numpy.fromstring(x,sep=";")
+dataset = numpy.fromstring(x, sep=";")
 dataset = numpy.vstack([dataset, dataset])
 
 print(dataset.size)
